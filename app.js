bd842467a4f5116ac91d13da09f3d9b81cf69118
Libo Jian <1220266538@qq.com>
2020-12-08 23:28:04 -0500

    fix bug PORT

--- app.js
+++ app.js
@@ -192,6 +192,8 @@ app.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-app.listen(3000, function() {
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
