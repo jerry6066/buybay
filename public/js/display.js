2f8658c6702422aa01d1c6e885d75070536412bb
Libo Jian <1220266538@qq.com>
2020-12-11 13:37:49 -0500

    add click item feature

--- public/js/display.js
+++ public/js/display.js
@@ -7,3 +7,8 @@ $('#next-btn').click(function() {
   var prev_page = parseInt($('#page_num').text()) + 1;
   window.location.pathname = "/display/page/" + prev_page.toString();
 })
+
+$('.item-card').click(function(event) {
+  var item_id = event.currentTarget.children[0].textContent;
+  window.location.href = window.location.href.split('/display')[0] + '/item/' + item_id;
+})
